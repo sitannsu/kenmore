3fb0182fbe2f64dcd2942007fcac61f71d6fd274
Bibhudutta Das <bibhudutta.das44@gmail.com>
2021-04-01 22:58:56 +0530

    friend.js

--- controllers/friend.js
+++ controllers/friend.js
@@ -16,7 +16,7 @@ exports.requestFriend =  (req, res) => {
                 const sender = await User.findOne({ _id: req.body.senderUserId });
                 console.log("api invoked");
                 var message = {
-                    app_id: "4caa225e-a77d-485c-bbc5-74e432aa6e2f",
+                    app_id: "2fda0b56-2f68-426c-8b70-8990d7817d1b",
                     contents: { "en": `${sender.name} sent you a friend request` },
                     include_player_ids: [receiver.playerId] //'deb66713-0913-461a-a330-a67edb5fafb4'
                 };
