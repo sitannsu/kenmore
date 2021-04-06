05dfff671c6b2ebb569a1d0a619bb98eee51c511
Sitansu Jena <sitansu.jena@transformco.com>
2021-04-06 13:44:29 +0530

    comment count issue fix

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,33 +1,66 @@
 const { result } = require('lodash');
 const notification = require('../models/notification');
 const Notification = require('../models/notification');
+const User = require('../models/user');
+var mongoose = require('mongoose');
+
 
 exports.sendNotifications = async (req, res) => {
 
-    const notification = new Notification(req.body);
-    notification.user = req.body.currentUserId;
-    notification.save((err, result) => {
-        if (err) {
+
+    User.findOne({ _id: req.body.user }, (err, user)=>{
+
+        if(!user){
             return res.status(400).json({
-                error: err
+                error: "User does not exist"
             });
         }
-        res.json(result);
-    });
+
+        const notification = new Notification(req.body);
+        //console.log("useruseruseruser",user);
+        notification.user = {_id:user._id , profileImageUrl:user.profileImageUrl, firstName:user.firstName,
+             lastName:user.lastName};
+        notification.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(result);
+        });
+    })
+
+
      
 };
 
-exports.getAllNotifications = async (req, res, userId) => {
-    Notification.find({currentUserId:userId})
-        .populate('user', '_id name profileImageUrl')
-      .then((data) => {
-        res.send(data);
-    })
-       .catch(err => {
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving notes."
+exports.getAllNotifications = async (req, res) => {
+    //console.log(req.params.userId.toString());
+    Notification.find({user: req.params.userId})
+        .populate('user', '_id firstName profileImageUrl')
+        .exec((err, notification) => {
+            
+            if (err || !notification) {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while retrieving notes."
+                });
+            }
+            //req.profile = user; 
+            res.status(200).json({
+                notification
             });
+            // adds profile object in req with user info
+            //next();
         });
+
+
+
+
+
+
+
+
+  
      
 };
 
