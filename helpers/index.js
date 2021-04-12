b8775bf6dd4b7bb7cef8485003e42c8f8c140595
Sitansu Jena <sitansu.jena@transformco.com>
2021-04-12 19:23:34 +0530

    email

--- helpers/index.js
+++ helpers/index.js
@@ -9,8 +9,8 @@ exports.sendEmail = emailData => {
     secure: false,
     requireTLS: true,
     auth: {
-      user: "bibhu.redmi@gmail.com",
-      pass: "8908901118"
+      user: "advancedcybernetic@gmail.com",
+      pass: "allinonetech2@"
     }
   });
   return transporter
